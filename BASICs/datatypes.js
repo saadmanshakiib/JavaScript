7010e3bb91b2aef131eb8043f7b66bacfb622f1b
Sadman Al Sakib <150785896+saadmanshakiib@users.noreply.github.com>
2025-04-07 19:18:38 +0600

    Add files via upload

---
+++ BASICs/datatypes.js
@@ -0,0 +1,19 @@
+console.log("data types check");
+
+    let a = 90;
+    var b = 88;
+    console.log(a+b);
+
+    let s = "sadman";
+    let s2 = "nissan";
+
+    console.log(s+" "+s2);
+
+    let empty = null;
+    console.log("empty value check : "+empty);
+
+     var check = true;
+     console.log(check);
+
+     
+
---
+++ BASICs/functions.js
@@ -0,0 +1,15 @@
+console.log("use of functions");
+console.log(sum(34,45))
+function sum( a, b){
+    return a+b;
+}
+
+    function greet(){
+        return "Welcome sadman";
+    }
+
+    console.log(greet);
+
+    
+
+    
