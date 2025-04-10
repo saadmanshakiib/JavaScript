7f1b38917ed71eee166b0e59085fa7477d3cc575
Sadman Al Sakib <150785896+saadmanshakiib@users.noreply.github.com>
2025-04-10 22:35:12 +0600

    Add files via upload

---
+++ HTML/1st.js
@@ -0,0 +1,15 @@
+console.log("Welcome to javascript");
+var a = 78;
+var b = 34;
+console.log("sum of a + b = "+ (a+b));
+
+for(var i=1;i<=10;i++){
+    console.log((i)+"th loop ");
+    if(i%2 == 0){
+        console.log("Even loop");
+    }
+    else {
+        console.log("Odd loop");
+    }
+}
+
---
+++ HTML/object.js
@@ -0,0 +1,15 @@
+let object = {
+    name : "sadman",
+    age : 22,
+    course :"CSE"
+}
+console.log(object);
+
+//creating object using new
+
+let obj = new Object();
+obj.name = "sakib";
+obj.age = 22;
+obj.course = "CoE";
+
+console.log(obj);
